ce1b15394b2bb09f8a1f6a9be8d13176ffa2727f
Jesus Magana <maganazero5@gmail.com>
2014-07-18 15:06:33 -0700

    Initial Commit

---
+++ GLParticles1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLParticles1
+//
+//  Created by Jesus Magana on 7/18/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GLParticles1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLParticles1
+//
+//  Created by Jesus Magana on 7/18/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
