8b37487e00568aaa4470fb499bbd5f94d184e95e
Jesus Magana <maganazero5@gmail.com>
2014-07-18 15:44:27 -0700

    Particles now shown

--- GLParticles1/MainViewController.m
+++ GLParticles1/MainViewController.m
@@ -8,6 +8,14 @@
 
 #import "MainViewController.h"
 #import "EmitterTemplate.h"
+#import "EmitterShader.h"
+
+@interface MainViewController ()
+
+// Properties
+@property (strong) EmitterShader* emitterShader;
+
+@end
 
 @implementation MainViewController
 
@@ -23,6 +31,9 @@
     GLKView* view = (GLKView*)self.view;
     view.context = context;
     
+    // Load Shader
+    [self loadShader];
+    
     // Load Particle System
     [self loadParticles];
     [self loadEmitter];
@@ -35,6 +46,31 @@
     // Set the background color (green)
     glClearColor(0.30f, 0.74f, 0.20f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    // 1
+    // Create Projection Matrix
+    float aspectRatio = view.frame.size.width / view.frame.size.height;
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakeScale(1.0f, aspectRatio, 1.0f);
+    
+    // 2
+    // Uniforms
+    glUniformMatrix4fv(self.emitterShader.uProjectionMatrix, 1, 0, projectionMatrix.m);
+    glUniform1f(self.emitterShader.uK, emitter.k);
+    
+    // 3
+    // Attributes
+    glEnableVertexAttribArray(self.emitterShader.aTheta);
+    glVertexAttribPointer(self.emitterShader.aTheta,                // Set pointer
+                          1,                                        // One component per particle
+                          GL_FLOAT,                                 // Data is floating point type
+                          GL_FALSE,                                 // No fixed point scaling
+                          sizeof(Particle),                         // No gaps in data
+                          (void*)(offsetof(Particle, theta)));      // Start from "theta" offset within bound buffer
+    
+    // 4
+    // Draw particles
+    glDrawArrays(GL_POINTS, 0, NUM_PARTICLES);
+    glDisableVertexAttribArray(self.emitterShader.aTheta);
 }
 
 - (void)loadParticles
@@ -44,11 +80,29 @@
         // Assign each particle its theta value (in radians)
         emitter.particles[i].theta = GLKMathDegreesToRadians(i);
     }
+    // Create Vertex Buffer Object (VBO)
+    GLuint particleBuffer = 0;
+    glGenBuffers(1, &particleBuffer);                   // Generate particle buffer
+    glBindBuffer(GL_ARRAY_BUFFER, particleBuffer);      // Bind particle buffer
+    glBufferData(                                       // Fill bound buffer with particles
+                 GL_ARRAY_BUFFER,                       // Buffer target
+                 sizeof(emitter.particles),             // Buffer data size
+                 emitter.particles,                     // Buffer data pointer
+                 GL_STATIC_DRAW);                       // Usage - Data never changes; used for drawing
 }
 
 - (void)loadEmitter
 {
     emitter.k = 4.0f;   // Constant k
+}
+
+#pragma mark - Load Shader
+
+- (void)loadShader
+{
+    self.emitterShader = [[EmitterShader alloc] init];
+    [self.emitterShader loadShader];
+    glUseProgram(self.emitterShader.program);
 }
 
 @end
